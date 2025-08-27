f51ae8ade54b5f65da49a1f2b6268eb4d6933caf
naveen6010 <naveen972821@gmail.com>
2025-08-27 15:16:35 +0530

    do some changes

--- clock.js
+++ clock.js
@@ -2,14 +2,17 @@ let hr = document.getElementById('hour');
 let min = document.getElementById('min');
 let sec = document.getElementById('sec');
 function displayTime(){
+    if(!hr||!min||!sec){
+        return;
+    }
     let date = new Date();
     // getting hour,min and sec from date
     let hh = date.getHours();
     let mm = date.getMinutes();
     let ss = date.getSeconds();
 
-    let hrotation = 30*hh + mm/2;
-    let mRotation = 6*mm;
+    let hrotation = 30*hh+mm*0.5+ss*(0.5/60);
+    let mRotation = 6*mm+ss*0.1;
     let sRotation = 6*ss;
     hr.style.transform = `rotate(${hrotation}deg)`;
     min.style.transform = `rotate(${mRotation}deg)`;
@@ -17,4 +20,5 @@ function displayTime(){
 
 
 }
+displayTime();
 setInterval(displayTime,1000);
